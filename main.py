f8eeb115f0a7b94868fb218c5e3bfb8fd1de0132
nymiora <hei.nymiora@gmail.com>
2025-07-30 13:45:00 +0300

    change user

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from starlette.responses import HTMLResponse
 app = FastAPI()
 
-@app.get("/hello")
+@app.get("/helo")
 def hello():
     with open("hello.html","r",encoding= "utf-8") as file:
         html_content =file.read()
