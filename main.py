d06de1ced59f087e177378436ba50faf0bacab76
nymiora <hei.nymiora@gmail.com>
2025-07-31 09:14:54 +0300

    question2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 
 from fastapi import FastAPI
-
+from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -10,3 +10,10 @@ app = FastAPI()
 @app.get("/ping")
 def ping():
     return {"pong"}
+
+
+@app.get("/home")
+def hello():
+    with open("hello.html","r",encoding= "utf-8") as file:
+        html_content =file.read()
+        return HTMLResponse(content=html_content)
