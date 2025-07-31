aef21b39569e79d6211df57014fc26adb0d6ed3f
nymiora <hei.nymiora@gmail.com>
2025-07-31 09:19:38 +0300

    question 4

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-
+from typing import List
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 from starlette.responses import HTMLResponse
 
 app = FastAPI()
@@ -17,3 +18,24 @@ def hello():
     with open("hello.html","r",encoding= "utf-8") as file:
         html_content =file.read()
         return HTMLResponse(content=html_content)
+
+
+
+class Posts(BaseModel):
+    author: str
+    title: str
+    content: str
+    creation_date: int
+
+@app.post("/posts")
+def add_posts(new_posts: List[Posts], posts=None):
+    added = 0
+    for post in new_posts:
+
+        if not any(s["Reference"] == post.Reference for s in posts):
+            posts.append(post.dict())
+            added += 1
+    return {
+        "message": f"{added} posts ajouté(s)",
+        "post": posts
+    }
